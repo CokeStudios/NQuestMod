f9a526491fa401d15a4ad66943c285122a1b90b9
zbx1425 <support@zbx1425.cn>
2023-10-13 19:44:55 +0800

    Initial Commit

---
+++ src/main/java/cn/zbx1425/nquestbot/NQuestBot.java
@@ -0,0 +1,13 @@
+package cn.zbx1425.nquestbot;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NQuestBot implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
